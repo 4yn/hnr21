91d2c08b976db2104f97bb626a21212e428434aa
Chang Si Yuan <changsiyuan1999@gmail.com>
2021-01-08 23:46:45 +0800

    Make body generation gender-specific

--- res/gen_parts.py
+++ res/gen_parts.py
@@ -6,27 +6,31 @@ import re
 def snake_to_camel(x):
     return "".join([i[0].upper() + i[1:] for i in x.split('_')])
 
-assetList = [
-    ('Bodies', glob('body/*.png')),
-    ('Faces', glob('face/*.png')),
-    ('Eyes', glob('eye/*.png')),
-    ('Noses', glob('nose/*.png')),
-    ('Mouths', glob('mouth/*.png')),
-    ('Hair', glob('hair/*.png')),
-]
+for gender in ['m', 'f']:
 
-for name, files in assetList:
-    rows = [
-        f"import {snake_to_camel(re.search(r'/(.+).png', file)[1])} from '../../res/{file}'"
-        for file in files
+    assetList = [
+        ('body', glob(f'body/*{gender}.png')),
+        ('face', glob(f'face/*{gender}.png')),
+        ('eyes', glob(f'eye/*{gender}.png')),
+        ('nose', glob(f'nose/*{gender}.png')),
+        ('mouth', glob(f'mouth/*{gender}.png')),
+        ('hair', glob(f'hair/*{gender}.png')),
     ]
 
-    print("\n".join(rows))
+    for name, files in assetList:
+        rows = [
+            f"import {snake_to_camel(re.search(r'/(.+).png', file)[1])} from '../../res/{file}'"
+            for file in files
+        ]
 
-    rows = [
-        f"\t{snake_to_camel(re.search(r'/(.+).png', file)[1])},"
-        for file in files
-    ]
-    print(f"export const {name} = [")
-    print("\n".join(rows))
-    print(f"]")
+        print("\n".join(rows))
+
+    
+    print(f"export const PersonParts{gender.upper()} = {{")
+    for name, files in assetList:
+        rows = [
+            f"{snake_to_camel(re.search(r'/(.+).png', file)[1])}"
+            for file in files
+        ]
+        print(f"\t{name.lower()}: [{', '.join(rows)}],")
+    print(f"}}")
